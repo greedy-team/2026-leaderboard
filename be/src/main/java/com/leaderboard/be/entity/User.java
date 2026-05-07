a1ffe61d473006561b43d62b7673a23b60531044
haeun92e0 <gracekim8729@gmail.com>
2026-05-07 18:06:33 +0900

    fix: nickname에 unique 추가

--- be/src/main/java/com/leaderboard/be/entity/User.java
+++ be/src/main/java/com/leaderboard/be/entity/User.java
@@ -19,7 +19,7 @@ public class User extends BaseEntity {
     @Column(name = "user_id", nullable = false, unique = true)
     private String userId;
 
-    @Column(name = "nickname", nullable = false)
+    @Column(name = "nickname", nullable = false, unique = true)
     private String nickname;
 
     @Column(name = "phone", unique = true)
