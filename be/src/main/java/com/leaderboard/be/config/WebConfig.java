31cf6b2af91aa7058f95834e37d4b7c575de9a26
htdufhc-bob <htdufhc@gmail.com>
2026-05-09 20:48:45 +0900

    feat: webConfig 추가

---
+++ be/src/main/java/com/leaderboard/be/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.leaderboard.be.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .maxAge(3600);
+    }
+}
