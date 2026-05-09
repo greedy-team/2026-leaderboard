323e035fdfca880746bff83ebf28a6d3bf8445b6
htdufhc-bob <htdufhc@gmail.com>
2026-05-09 18:24:06 +0900

    refactor: 피드백 리팩토링

--- be/deploy.sh
+++ be/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# 환경변수 로드
+source .env.deploy
+
+ECR_REGISTRY="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+ECR_IMAGE="${ECR_REGISTRY}/${ECR_REPO}:latest"
+
 # ECR 로그인
-aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 985818273436.dkr.ecr.ap-northeast-2.amazonaws.com
+aws ecr get-login-password --region "$AWS_REGION" | \
+  docker login --username AWS --password-stdin "$ECR_REGISTRY"
 
 # 최신 이미지 pull
-docker pull 985818273436.dkr.ecr.ap-northeast-2.amazonaws.com/leaderboard:latest
+docker pull "$ECR_IMAGE"
 
 # 기존 컨테이너 내리고
 docker compose -f docker-compose-prod.yml down app
