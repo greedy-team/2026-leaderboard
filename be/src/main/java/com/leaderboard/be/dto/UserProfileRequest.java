fa66ee87e389a0fc529bcc85a9b9f45439a51cef
HyerimH <gofla452@naver.com>
2026-05-18 00:25:57 +0900

    feat: 닉네임 8자 이하 유효성 검사 추가 및 swagger 명세 업데이트

--- be/src/main/java/com/leaderboard/be/dto/UserProfileRequest.java
+++ be/src/main/java/com/leaderboard/be/dto/UserProfileRequest.java
@@ -2,8 +2,9 @@ package com.leaderboard.be.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserProfileRequest(
-        @NotBlank String nickname,
+        @NotBlank @Size(max = 8, message = "닉네임은 8자 이하여야 합니다.") String nickname,
         @Pattern(regexp = "^\\d{8}$", message = "전화번호는 8자리 숫자여야 합니다.") String phone
 ) {}
